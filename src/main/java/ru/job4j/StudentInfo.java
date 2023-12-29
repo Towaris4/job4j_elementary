b32cfc4bf39ae52df10fdcf3d357dc4c8cd70cc6
Towaris4 <ivan.tarasov2488@gmail.com>
2023-12-29 16:56:39 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Ivan Tarasov");
         System.out.println("24.02.1998");
     }
