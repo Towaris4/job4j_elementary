3a9042549e8c717f3801809be65aaf3ea87f972f
Towaris4 <ivan.tarasov2488@gmail.com>
2024-01-03 20:50:28 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Sergey Abramov";
+        names[1] = "Ivan Tarasov";
+        names[2] = "Sergey Kyznetsov";
+        names[3] = "Kirill Danilov";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
